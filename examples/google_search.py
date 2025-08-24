3f441a1e3aececf1462f0bc4642c32807b7d87a3
Glytantio <lkd11242000@gmail.com>
2025-08-25 05:38:11 +0800

    Update google_search.py

--- examples/google_search.py
+++ examples/google_search.py
@@ -3,18 +3,25 @@ from selenium.webdriver.common.By import By
 from selenium.webdriver.common.Keys import Keys
 import time
 
+# Initialize Chrome and renaming the page title
 def setup():
   driver = webdriver.Chrome()
   driver.get("https://www.google.com")
   assert "Google" in driver.title
 
+# Waiting for 3s to show results before closing the browser and also printing the page title
 def teardown():
 time.sleep(3)
 print("Page Title is:", driver.title)
 driver.quit()
 
-setup()
-search_box = driver.find_element(BY.NAME,"q")
-search_box.send_keys("Selenium Python")
-search_box.send_keys(Keys.RETURN)
-teardown()
+try:
+  setup()
+  # Find the search box and type a query
+  search_box = driver.find_element(BY.NAME,"q")
+  search_box.send_keys("Selenium Python")
+  # Hits the enter key on your keyboard
+  search_box.send_keys(Keys.RETURN)
+
+finally:
+  teardown()
