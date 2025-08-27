1f652620e41cab2891507e837cafb3adf249c499
Glytantio <lkd11242000@gmail.com>
2025-08-28 04:46:01 +0800

    Update handling-alerts.py

--- examples/handling-alerts.py
+++ examples/handling-alerts.py
@@ -4,14 +4,51 @@ import time
 
 def setup():
   driver = webdriver.Chrome()
-  driver.get("https://the-internet.herokuapp.com/javascript_alerts")
   return driver
+
+def pressOK():
+  # This function is for pressing the ok button whenever an alert is displayed
+  alert = driver.switch_to.alert
+  print = ("Alert text:", alert.text)
+  alert.accept()
+  
+# Waiting for 3s to show results before closing the browser and also printing the page title
+def teardown():
+time.sleep(3)
+print("Page Title is:", driver.title)
+driver.quit()
 
 def handling_alerts():
   driver = setup()
   try:
+    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
     # Find the first button and click
     btn = driver.find_element(By.XPATH, "//button[text()='Click for JS Alert']")
     btn.click()
-
+    
     time.sleep(2)  # wait to see the alert
+    
+    pressOK() # Calls the function to click the ok button for every alert
+
+    btn2 = driver.find_element(BY.XPATH, "//button[text()='Click for JS Alert']")
+    btn2.click()
+
+    time.sleep(2)
+
+    pressOK()
+
+    btn3 = driver.find_element(By.XPATH, "//button[text()='Click for JS Confirm']")
+    btn3.click()
+
+    time.sleep(2)
+
+    alert = driver.switch_to.alert
+    print = ("Alert text:", alert.text)
+    alert.send_keys("Hello Glytantio!")
+    alert.accept()
+
+  finally:
+    teardown()
+
+if
+  
