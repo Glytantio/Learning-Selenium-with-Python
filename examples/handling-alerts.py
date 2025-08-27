2e574e7a0cee124c0824fcb2a8e1129b207c3fe3
Glytantio <lkd11242000@gmail.com>
2025-08-28 04:50:59 +0800

    Update handling-alerts.py

--- examples/handling-alerts.py
+++ examples/handling-alerts.py
@@ -30,26 +30,31 @@ def handling_alerts():
     
     pressOK() # Calls the function to click the ok button for every alert
 
+    # Find the second button and click
     btn2 = driver.find_element(BY.XPATH, "//button[text()='Click for JS Alert']")
     btn2.click()
 
     time.sleep(2)
 
     pressOK()
-
+    
+    # Find the third button and click
     btn3 = driver.find_element(By.XPATH, "//button[text()='Click for JS Confirm']")
     btn3.click()
 
     time.sleep(2)
-
+    
     alert = driver.switch_to.alert
     print = ("Alert text:", alert.text)
+    # Type something inside the confirm alert and enters it.
     alert.send_keys("Hello Glytantio!")
     alert.accept()
 
   finally:
+    # Closes the browser
     teardown()
 
 if __name__ == "__main__":
+  # Initiate the automation script
   handling_alerts()
   
